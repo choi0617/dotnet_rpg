6932b91932af35e5966485f8551679d34d5a378f
Sam <16262606+choi0617@users.noreply.github.com>
2021-11-29 09:13:44 -0500

    added controller actions

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
@@ -7,6 +10,31 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character Knight = new Character();
+
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
     }
 }
