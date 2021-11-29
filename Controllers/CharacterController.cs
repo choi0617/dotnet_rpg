025cf2a63f5d7f3f4bc32d73062120dc7827c867
Sam <16262606+choi0617@users.noreply.github.com>
2021-11-29 08:21:24 -0500

    initial commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,12 @@
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character Knight = new Character();
+    }
+}
